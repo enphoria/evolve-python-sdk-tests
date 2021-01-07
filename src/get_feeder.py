b7276b7ce3818f9b9053e534cdeaba56679997d1
Carlos <carlos.macana@zepben.com>
2021-01-07 19:51:11 +1100

    Adjust to recieve command line Arguments in get_feeder.py
    Signed-off-by: Carlos <carlos.macana@zepben.com>

--- src/get_feeder.py
+++ src/get_feeder.py
@@ -1,19 +1,44 @@
 import asyncio
+import sys
+import getopt
 
 from zepben.evolve.streaming import connect_async, NetworkConsumerClient
 from zepben.evolve import NetworkService, Equipment
 
 
-async def main():
-    async with connect_async(host="localhost", rpc_port=50052) as channel:
+def print_feeder_eq(service):
+    for equip in service.objects(Equipment):
+        print(equip.mrid, equip.name)
+
+
+async def main(argv):
+    feeder_mrid = ''
+    rpc_port = 50052
+    host = "localhost"
+    try:
+        opts, args = getopt.getopt(argv, "h:i:p:u:", ["mrid=", "port=", "host="])
+    except getopt.GetoptError:
+        print('get_feeder.py -i <feeder_mrid> -p <rpc_port> -u <host>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('get_feeder.py -p <rpc_port> -i <feeder_mrid>')
+            sys.exit()
+        elif opt in ("-i", "--mrid"):
+            feeder_mrid = arg
+        elif opt in ("-p", "--port"):
+            rpc_port = arg
+        elif opt in ("-u", "--host"):
+            host = arg
+    async with connect_async(host=host, rpc_port=rpc_port) as channel:
         service = NetworkService()
         client = NetworkConsumerClient(channel)
-        result = (await client.get_feeder(service, 'CPM3B3')).throw_on_error()
-        print(service.get('CPM3B3'))
+        result = (await client.get_feeder(service, feeder_mrid)).throw_on_error()
+        print(feeder_mrid)
+        print(service.get(feeder_mrid))
+        print_feeder_eq(service)
 
-#        for equip in service.objects(Equipment):
-#            print(equip.mrid, equip.name)
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(main(sys.argv[1:]))
